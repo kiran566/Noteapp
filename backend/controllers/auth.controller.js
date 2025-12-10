522bb4376cea27f4b07b57bf274bb3c0b4f5e34c
kiran566 <kiranbasava230@gmail.com>
2025-12-10 13:48:07 +0530

    login route added

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -35,3 +35,39 @@ export const signup = async (req, res) => {
     res.status(500).json({ message: "Server error" });
   }
 };
+
+
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // 1. Check if user exists
+    const user = await User.findOne({ email });
+    if (!user) return res.status(404).json({ message: "User not found" });
+
+    // 2. Compare password
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(400).json({ message: "Invalid password" });
+
+    // 3. Generate JWT token
+    const token = jwt.sign(
+      { id: user._id },
+      process.env.JWT_SECRET,
+      { expiresIn: "7d" }
+    );
+
+    res.status(200).json({
+      message: "Login successful",
+      token,
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email
+      }
+    });
+
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
