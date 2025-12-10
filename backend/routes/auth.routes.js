8fdf649dd5ee7838409105ed73f960a1157a0539
kiran566 <kiranbasava230@gmail.com>
2025-12-10 14:08:51 +0530

    dashboard

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { signup,login } from "../controllers/auth.controller.js";
+import { protect } from "../middleware/auth.middleware.js";
 
 
 const router = express.Router();
@@ -7,6 +8,13 @@ const router = express.Router();
 // Signup route
 router.post("/signup", signup);
 router.post("/login", login);
+router.get("/dashboard", protect, (req, res) => {
+  res.json({
+    message: "Welcome to your dashboard!",
+    userId: req.user.id,
+  });
+});
+
 
 
 export default router;
